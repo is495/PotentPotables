75c759a4ffc863c659cb8b55f81f5fe69150943d
Christopher Meyers <chrismeyers94@gmail.com>
2016-09-15 16:18:14 -0700

    first draft at symbols

--- public/bootstrap-assets/js/cocktails.js
+++ public/bootstrap-assets/js/cocktails.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
 })
 function myFunction() {
   $("li").removeClass('green');
+  $("img").removeClass('all');
+  $("img").removeClass('some');
+  $("img").removeClass('none');
+
   location.href = "#portfolio";
   var ingredients= $('#sbTwo').val();  var n = ingredients.includes("vodka");
   if (n==true) {    $( ".vodka" ).addClass(function( index ) {
@@ -94,6 +98,7 @@ $( ".cherries" ).addClass(function( index ) {
   return "red" ;
 });
 }
+
 var n = ingredients.includes("whiskey");
 if (n==true) {
   $( ".whiskey" ).addClass(function( index ) {
@@ -105,16 +110,65 @@ $( ".whiskey" ).addClass(function( index ) {
   return "red" ;
 });
 }
-var n = ingredients.includes("vodka,vermouth,olives");
-if (n==true) {
-  $( ".martini" ).addClass(function( index ) {
+// martini start
+var n = ingredients.includes("bourbon");
+var v = ingredients.includes("bitters");
+var s = ingredients.includes("sugar");
+
+if ((n==true) && (v==true)&& (s==true)) {
+  $( ".old-all" ).addClass(function( index ) {
+    return "all" ;
+  });
+}
+else if ((n==true) && (v==true)){
+$( ".old-some" ).addClass(function( index ) {
+  return "some" ;
+});
+}
+else {
+$( ".old-none" ).addClass(function( index ) {
+  return "none" ;
+});
+}
+// manhattan start
+var n = ingredients.includes("whiskey");
+var v = ingredients.includes("bitters");
+var s = ingredients.includes("vermouth");
+
+if ((n==true) && (v==true)&& (s==true)) {
+  $( ".man-all" ).addClass(function( index ) {
+    return "all" ;
+  });
+}
+else if ((n==true) && (v==true)){
+$( ".man-some" ).addClass(function( index ) {
+  return "some" ;
+});
+}
+else {
+$( ".man-none" ).addClass(function( index ) {
+  return "none" ;
+});
+}
+
+// martini start
+var n = ingredients.includes("vodka");
+var v = ingredients.includes("vermouth");
+if ((n==true) && (v==true)) {
+  $( ".martini-all" ).addClass(function( index ) {
     return "all" ;
   });
 }
 else {
-$( ".martini" ).addClass(function( index ) {
+$( ".martini-none" ).addClass(function( index ) {
   return "none" ;
 });
 }
+
+
+// martini end
+// martini start
+
+// martini end
 location.href = "#portfolio";
 }
